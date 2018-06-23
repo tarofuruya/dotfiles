4e2505b93d2e2d98f699a9883fa864881367f91a
Taro Furuya <taro.furuya@gmail.com>
2018-06-23 18:11:34 +0900

    Added VSCode settings

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# dotfiles
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
     ln -snfv ~/dotfiles/$f ~/$f    
 done
+
+# VSCode
+cd ~/Library/Application\ Support/Code/User/
+rm settings.json
+ln -s ~/dotfiles/vscode/settings.json ./settings.json
+cd ~/dotfiles/vscode
+bash install_extensions.sh
+
 echo Installed!
---
+++ vscode/install_extensions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cat ./extensions | while read line
+do
+  code --install-extension $line
+done
